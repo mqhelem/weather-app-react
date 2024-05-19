cb57d3300748953ec708c222d1a7a99de7773367
mqhelem <155308250+mqhelem@users.noreply.github.com>
2024-05-19 18:51:58 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <h1>React Weather Search Engine</h1>
-      </div>
+      <Weather />
+      <footer>
+        Coded by <a href="https://github.com/mqhelem"> Nomqhele Mhlanga,</a>
+        source code hosted in
+        <a href="https://github.com/mqhelem/weather-app-react"> Github,</a>
+        app available at
+        <a href="https://weatherfourcast.netlify.app/"> Netlify</a>
+      </footer>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Weather() {
+  return (
+    <div>
+      <div>
+        <h1>React Weather Search Engine</h1>
+        <btn btn-primary>Search city</btn>
+      </div>
+    </div>
+  );
+}
