a2a45fbd810391a2f0bb3077250bdb093289d515
mqhelem <155308250+mqhelem@users.noreply.github.com>
2024-05-19 17:37:28 +0200

    installed dependencies

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div>
+        <h1>React Weather Search Engine</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
